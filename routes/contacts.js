6cb1d896ab932553895b9efa419b1db8441ebb7d
CaptainCarnivore <ausjensen0451@byui.edu>
2024-03-07 17:13:43 -0700

    broke stuff

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,6 +5,12 @@ const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll);
 
-router.get('/:id', contactsController.getSingle)
+router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateUser);
+
+router.delete('/:id', contactsController.deleteUser);
 
 module.exports = router;
